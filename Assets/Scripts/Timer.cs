0964448f59c3c4ac24b4a13ec705de93fcdbf932
Tom Williams <tomdanielwilliams@gmail.com>
2022-06-16 15:22:30 +0100

    General Changes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,34 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
     public Text timerText;
-    private float startTime;
-    private bool finnished = false;
+    //private float startTime;
+    float timePassed;
+    float levelTargetTime;
 
-    // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
+        //startTime = Time.time;
+
+        //Set level clear time target
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "Level1Scene") { levelTargetTime = 10f; }
+        if (currentScene.name == "Level2Scene") { levelTargetTime = 15f; }
+        if (currentScene.name == "Level3Scene") { levelTargetTime = 20f; }
     }
 
-    // Update is called once per frame
-    void Update() //Timer
+    void Update()
     {
-        if (GameObject.FindGameObjectWithTag("Human") == null)
-            return;
+        if ((GameObject.FindGameObjectsWithTag("Human").Length > 0) && (GameObject.FindGameObjectsWithTag("Zombie").Length > 0))
+        {
+            //Timer
+            //float t = Time.time - startTime;
 
-        float t = Time.time - startTime;
+            //string minutes = ((int)t / 60).ToString();
+            //string seconds = (t % 60).ToString("f2");
 
-        string minutes = ((int) t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
-        
-        timerText.text = minutes + ":" + seconds;
-    }
+            //timerText.text = minutes + ":" + seconds;
 
-    public void Finnish() //Change text colour on finish
-    {
-        timerText.color = Color.red;
+            //Timer
+            timePassed += Time.deltaTime;
+
+            if (timePassed >= 60f)
+            {
+                float seconds = timePassed % 60;
+                int minutes = (int)timePassed / 60;
+                timerText.text = minutes.ToString() + ":" + seconds.ToString("f2");
+            }
+            else
+            {
+                float seconds = timePassed;
+                int minutes = 0;
+                timerText.text = minutes.ToString() + ":" + seconds.ToString("f2");
+            }
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Human").Length <= 0)
+        {
+            if (timePassed <= levelTargetTime)
+            {
+                //Win
+                Debug.Log("Level Beat");
+            }
+            else
+            {
+                //Lose
+                Debug.Log("Level Not Beat, Target Time: 00:" + levelTargetTime.ToString());
+            }
+        }
     }
 }
